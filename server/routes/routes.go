e70d4fc9ed86ee57f4cfeb17e074a6927f11c333
cs Wang <csw.dawn@gmail.com>
2020-04-03 22:43:34 +0800

    add swagger doc

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -3,12 +3,18 @@ package routes
 import (
 	v1 "gin-vegan-map-api/server/api/v1"
 
+	_ "gin-vegan-map-api/docs"
+
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 // Engine - engine
 func Engine() *gin.Engine {
 	r := gin.Default()
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	ver1 := r.Group("/api/v1")
 	{
